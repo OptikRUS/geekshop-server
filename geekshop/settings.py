49745a9761e46dc3229d926ebf957790647b02be
optikrus <optikrus61@gmail.com>
2022-02-09 13:17:07 +0300

    edit USE_L10N and USE_THOUSAND_SEPARATOR

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -104,12 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if USE_L10N:
+    USE_THOUSAND_SEPARATOR = True
