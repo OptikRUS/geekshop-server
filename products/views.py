fc24ebf5dee23a58ef8dc67b60bf629493414665
optikrus <optikrus61@gmail.com>
2022-02-12 00:39:02 +0300

    add categories for products controller and template

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, ProductCategory
 
 
 def index(request):
@@ -11,8 +11,11 @@ def index(request):
 
 def products(request):
     all_products = Product.objects.all()
+    categories = ProductCategory.objects.all()
     context = {
         'title': 'продукты',
         'products': all_products,
+        'categories': categories,
     }
     return render(request, 'products/products.html', context)
+
