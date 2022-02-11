193b3c697904d5fd785f8d3a12b933b826662a27
optikrus <optikrus61@gmail.com>
2022-02-11 21:38:36 +0300

    add ProductCategory and Product with migrations

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField('название категории', max_length=64, unique=True)
+    description = models.TextField('описание', blank=True, null=True)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField('название товара', max_length=256, unique=True)
+    image = models.ImageField('изображение товара', upload_to='products_images', blank=True, null=True)
+    description = models.CharField('описание', max_length=256)
+    price = models.DecimalField('цена', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField('количество на складе', default=0)
