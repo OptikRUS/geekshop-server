eca279902a512f55c2a6bead72f0d578d9b33f7e
optikrus <optikrus61@gmail.com>
2022-02-11 22:17:00 +0300

    add __str__ for ProductCategory and Product

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField('название категории', max_length=64, unique=True)
     description = models.TextField('описание', blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
@@ -13,3 +16,6 @@ class Product(models.Model):
     description = models.CharField('описание', max_length=256)
     price = models.DecimalField('цена', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField('количество на складе', default=0)
+
+    def __str__(self):
+        return f'Продукт {self.name} | Категория: {self.category.name}'
