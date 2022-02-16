fe1dad28296b3f88004ec6f8f3ee8c51a8c483a8
optikrus <optikrus61@gmail.com>
2022-02-17 01:01:48 +0300

    add login and registration controllers

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    context = {
+        'title': 'авторизация',
+    }
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {
+        'title': 'регистрация',
+    }
+    return render(request, 'users/register.html', context)
