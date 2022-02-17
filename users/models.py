6410fc98adc27eafbc4aeddea45d9366df7f9f19
optikrus <optikrus61@gmail.com>
2022-02-17 03:45:45 +0300

    add age to users model

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ class User(AbstractUser):
     image = models.ImageField('аватар', upload_to='users_images', null=True, blank=True)
     telegram_id = models.PositiveIntegerField('телеграм ID', unique=True, null=True, blank=True)
     telegram_username = models.CharField('ник телеграм', max_length=64, null=True, blank=True)
+    age = models.PositiveIntegerField('возраст', null=True)
 
     # def save(self, *args, **kwargs):
     #     self.telegram_username = f'@{self.telegram_username}'
