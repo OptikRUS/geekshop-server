aac43b10988910d2d6af82898dbd18692f119d28
optikrus <optikrus61@gmail.com>
2022-02-17 03:37:16 +0300

    add logout controller, url and template logic

--- users/views.py
+++ users/views.py
@@ -41,3 +41,8 @@ def registration(request):
         'form': form
     }
     return render(request, 'users/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
