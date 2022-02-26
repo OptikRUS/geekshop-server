70db968a66cdcbc806014c4a59a05993e6bb4b82
optikrus <optikrus61@gmail.com>
2022-02-26 14:50:56 +0300

    add create controller to admins

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
 
 
 def index(request):
@@ -11,7 +13,21 @@ def index(request):
 def admin_users(request):
     users = User.objects.all()
     context = {
-        'title': 'Админ-панель/Пользователи',
+        'title': 'Админка/Пользователи',
         'users': users,
     }
     return render(request, 'admins/admin-users-read.html', context)
+
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'title': 'Админка/Создание пользователя', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
