cf500ff4c21e9d2f22c3835860101c612e278b6b
optikrus <optikrus61@gmail.com>
2022-02-26 12:34:25 +0300

    add index controller to admins

---
+++ admins/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from admins.views import index
+
+app_name = 'admins'
+
+urlpatterns = [
+    path('', index, name='index')
+]
+
+
--- admins/views.py
+++ admins/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {'title': 'Админ-панель'}
+    return render(request, 'admins/index.html', context)
