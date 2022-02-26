97ec8744ffc8869b25b9daf8c9d2eb15d4f1943a
optikrus <optikrus61@gmail.com>
2022-02-26 18:07:40 +0300

    add check permissions

--- admins/views.py
+++ admins/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
+@user_passes_test(lambda user: user.is_staff)
 def index(request):
     context = {'title': 'Админ-панель'}
     return render(request, 'admins/index.html', context)
 
 
+@user_passes_test(lambda user: user.is_staff)
 def admin_users(request):
     users = User.objects.all()
     context = {
@@ -20,6 +23,7 @@ def admin_users(request):
     return render(request, 'admins/admin-users-read.html', context)
 
 
+@user_passes_test(lambda user: user.is_staff)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
@@ -35,6 +39,7 @@ def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html', context)
 
 
+@user_passes_test(lambda user: user.is_staff)
 def admin_users_update(request, pk):
     selected_user = User.objects.get(id=pk)
     if request.method == 'POST':
@@ -49,6 +54,7 @@ def admin_users_update(request, pk):
     return render(request, 'admins/admin-users-update-delete.html', context)
 
 
+@user_passes_test(lambda user: user.is_staff)
 def admin_users_delete(request, pk):
     user = User.objects.get(id=pk)
     user.safe_delete()
