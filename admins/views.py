8457de44e91fd48d4e152f2f949528516b94fb59
optikrus <optikrus61@gmail.com>
2022-02-26 13:22:45 +0300

    add read controller to admins

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+from users.models import User
+
 
 def index(request):
     context = {'title': 'Админ-панель'}
     return render(request, 'admins/index.html', context)
+
+
+def admin_users(request):
+    users = User.objects.all()
+    context = {
+        'title': 'Админ-панель/Пользователи',
+        'users': users,
+    }
+    return render(request, 'admins/admin-users-read.html', context)
