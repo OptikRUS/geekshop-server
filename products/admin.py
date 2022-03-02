bb68a9279994c1703611d6e892f911ab8f0b23e5
optikrus <optikrus61@gmail.com>
2022-03-02 20:55:21 +0300

    add pagination for products

--- products/admin.py
+++ products/admin.py
@@ -3,13 +3,10 @@ from django.contrib import admin
 from products.models import ProductCategory, Product
 
 
-
-
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'quantity')
     fields = ('name', 'description', ('category', 'price', 'quantity'), 'image',)
-    readonly_fields = ('description', )
     search_fields = ('name', )
     ordering = ('price', )
 
--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from products.models import Product, ProductCategory
 
 
@@ -9,16 +11,23 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     if category_id:
         products = Product.objects.filter(category_id=category_id)
     else:
         products = Product.objects.all()
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
 
     context = {
         'title': 'продукты',
         'categories': ProductCategory.objects.all(),
-        'products': products,
+        'products': products_paginator,
     }
     return render(request, 'products/products.html', context)
 
